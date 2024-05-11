356a01c78a752a3923f9de12515c9f546e5c627f
si-zhen <2080767743@qq.com>
2024-05-11 11:10:20 +0800

    numpy练习

---
+++ Andrew Ng_DLStudy/numpy_Pract/numpy_03.py
@@ -0,0 +1,46 @@
+# Author:司震
+# Create: 2024/5/9
+# Description:numpy函数
+import  numpy as np
+
+# test = np.char.add(['hello'],['world'])#依次对两个数组的元素进行字符串拼接
+# print(test)
+# test1 = np.char.add(["1,2,3"],["4,5,6"])
+# print(test1)
+
+# print(np.char.multiply('abc',3)) #多重连接字符串
+# print(np.char.center('abc',20,fillchar='-')) #指定长度20，填充字符为-
+# print(np.char.capitalize('abc')) #将字符串的第一个字母转为大写
+# print(np.char.title("wo shi sha dan")) #将字符串里每个单词的首字母大写
+# print(np.char.lower(['LAOPO',"LAOPO"]))
+
+# print(np.char.split('i like you?'))#默认分隔符为空格
+# print(np.char.split('www.baidu.com',sep='.'))#指定分隔符
+# print(np.char.join(':','baidu'))#指定分隔符连接数组中的元素或字符串
+# print(np.char.join([':','-'],['runoob','google']))#指定多个分隔符操作数组元素
+#print(np.char.replace('i like you','you','myself'))#是用新字符替换字符串中的所有字符串
+
+# a = np.array([0.49,5.55,123,0.567,25.532])
+# print(np.around(a))#舍入函数，返回指定数字的四舍五入值
+# print(np.ceil(a))#ceil,字面意思，向上取整
+
+# a = np.array([10,100,20])
+# print(np.power(a,2))#左边是数组，右边是幂
+
+# a = np.arange(12).reshape((3,4))
+# print(a)
+# print(a.T)
+
+# a = np.empty((2,2))#生成一个随机矩阵
+# print(a)
+
+# a =np.identity(5,dtype=np.int32)#生成一个单位矩阵
+# print(a)
+
+
+
+
+
+
+
+
